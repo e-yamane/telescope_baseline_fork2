a8a626a4cac1df7d19865da6e25176da53353b55
Hajime Kawahara <divrot@gmail.com>
2022-04-27 14:46:43 +0900

    convex completed

--- src/telescope_baseline/mapping/aperture.py
+++ src/telescope_baseline/mapping/aperture.py
@@ -1,12 +1,59 @@
+import numpy as np
+from healpy.pixelfunc import ang2vec, vec2ang
+
+def vec2ring(x):
+    """convert a vector to a ring
+    Args:
+        vertex list (vertex, coodinate)
+
+    Returns:
+        ring (edge, left-right, coordinate)
+
+    """
+    Nvertex, Ncoordinate=np.shape(x)
+    if Nvertex%2 == 1:
+        print("Nvertex needs to be even")
+    rs=(int(Nvertex/2),2,Ncoordinate)
+    x1=x.reshape(rs) 
+    x0=np.roll(x,1,axis=0).reshape(rs)
+    return np.hstack([x0,x1]).reshape((Nvertex,2,Ncoordinate))
+
 def cos_angle_from_normal_vectorAB(a,b,x):
-  """
-  Args:
-      a: 3D vetcor of A
-      b: 3D vector of B
-      x: vectors whose cos angle from normal vector of the ABO plane to be computed
+    """cos angle between the normal vector defined by a, b and x
+    Args:
+       a: 3D vetcor of A
+       b: 3D vector of B
+       x: vectors whose cos angle from normal vector of the ABO plane to be computed
+     
+    Returns:
+       cos_angles
+    """
+    return x@np.cross(a,b) 
 
-  Returns:
-      cos_angles
 
-  """
-  return x@np.cross(a,b)
+def convex_on_sphere(angv,angw):
+    """checking if the points 
+    Args:
+        angv: list of theta,phi points that defines the convex on a sphere
+        angw: theta,phi lists in or out of the convex on a sphere
+
+    Returns:
+        1 (in convex) or 0 (out of convex)
+    """
+
+    v=ang2vec(angv[0],angv[1])
+    w=ang2vec(angw[0],angw[1])
+    Nvertex,Ncoordinate=np.shape(v)
+    ring=vec2ring(v)
+    cosa=[]
+    for i in range(0,Nvertex):
+        cosa.append(cos_angle_from_normal_vectorAB(ring[i,0,:],ring[i,1,:],w))
+    cosa = np.array(cosa)
+    mask=(cosa>=0.0)*(cosa<=1.0)
+    return np.prod(mask,axis=0)
+
+
+
+    
+#if __name__ == "__main__":
+#    test_convex_on_sphere_map()
--- tests/mapping/test_aperture.py
+++ tests/mapping/test_aperture.py
@@ -1,8 +1,48 @@
-from telescope_baseline.mapping.aperture import cos_angle_from_normal_vectorAB
+from telescope_baseline.mapping.aperture import cos_angle_from_normal_vectorAB, vec2ring, convex_on_sphere
+from healpy.pixelfunc import vec2ang
+import numpy as np
 import pytest
 
 def test_cos_angle_from_normal_vectorAB():
-  a=np.array([1,0,0])
-  b=np.array([0,1,0])
-  v=cos_angle_from_normal_vectorAB(a,b,[ np.cross(a,b)])
-  assert v[0]==1
+    a=np.array([1,0,0])
+    b=np.array([0,1,0])
+    v=cos_angle_from_normal_vectorAB(a,b,[ np.cross(a,b)])
+    assert v[0]==1
+
+def test_vec2ring():
+    a=np.array([[1,1,1],[2,2,2],[3,3,3],[4,4,4],[5,5,5],[6,6,6]])
+    assert np.sum(vec2ring(a)[0,0,:]) == 6*3
+    assert np.sum(vec2ring(a)[0,1,:]) == 1*3
+    assert np.sum(vec2ring(a)[1,0,:]) == 1*3
+    assert np.sum(vec2ring(a)[1,1,:]) == 2*3
+
+def test_convex_on_sphere():
+    ex=np.array([1,0,0])
+    ey=np.array([0,1,0])
+    ea=np.array([0,1.0/np.sqrt(2.0),1.0/np.sqrt(2.0)])
+    eb=np.array([1.0/np.sqrt(2.0),0,1])
+    v=np.array([ex,ey,ea,eb])
+    angv=np.array(vec2ang(v)) # (Nvertex, theta-phi)
+    
+    w=np.array([0,0,1])
+    angw=np.array(vec2ang(w)).T[0] # (Nvertex, theta-phi)
+    assert convex_on_sphere(angv,angw)==0
+
+    w=np.array([1/np.sqrt(2.0),1/np.sqrt(2.0),1/np.sqrt(4.0)])
+    angw=np.array(vec2ang(w)).T[0] 
+    assert convex_on_sphere(angv,angw)==1
+
+def test_convex_on_sphere_map():
+    ex=np.array([1,0,0])
+    ey=np.array([0,1,0])
+    ea=np.array([0,1.0/np.sqrt(2.0),1.0/np.sqrt(2.0)])
+    eb=np.array([1.0/np.sqrt(2.0),0,1])
+    v=np.array([ex,ey,ea,eb])
+    angv=np.array(vec2ang(v)) # (theta-phi, Nvertex)
+    
+    w=np.array([0,0,1])
+    q=np.array([1/np.sqrt(2.0),1/np.sqrt(2.0),1/np.sqrt(4.0)])
+    v=np.array([w,q,w,q])
+    angw=np.array(vec2ang(v))
+    ans=convex_on_sphere(angv,angw)
+    assert (np.sum((ans - [0,1,0,1])**2)) == 0
