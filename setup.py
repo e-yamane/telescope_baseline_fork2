50a151fb5828f2f2d86a6a6d61e903b9f9020f95
Yoshiyuki Yamada <yamada.yoshiyuki@gmail.com>
2022-05-10 17:53:36 +0900

    In windows, default encoding of Shift_JIS (cp932) is assumed. Add encoding option in read method.

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ if __name__ == '__main__':
         url=find_meta('uri'),
         license=find_meta('license'),
         description=find_meta('description'),
-        long_description=open('README.md').read(),
+        long_description=open('README.md', encoding="utf-8").read(),
         long_description_content_type='text/markdown',
         packages=PACKAGES,
         package_dir={'': 'src'},
