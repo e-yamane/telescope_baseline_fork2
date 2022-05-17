ea1728b89493e97de2987b51bf7abee1984c3c17
kataza <kataza@ir.isas.jaxa.jp>
2022-05-18 07:56:54 +0900

    wl_eff.py の WlEfic クラスを efficiency.py の Efficiency クラスに変更他

--- src/telescope_baseline/dataclass/efficiency.py
+++ src/telescope_baseline/dataclass/efficiency.py
@@ -2,34 +2,34 @@ import dataclasses
 import numpy as np
 import json
 
+
 @dataclasses.dataclass(frozen=True)
-class WlEfic:
-    """
-    This class defines the wavelength dependent efficiency
+class Efficiency:
+    """This class defines the wavelength dependent efficiency.
 
     Attributes:
         wavelength (ndarray): Wavelengths in micron.
         efficiency (ndarray): Efficiencies.
-        title (str): Title of the data
-        comment (str): Comments
+        title (str): Title of the data.
+        comment (str): Comments.
     """
-    wavelength: np.ndarray = np.array([0.9,1.6])
-    efficiency: np.ndarray = np.array([0.85, 0.85]) # sample data
+    # set sample data as default
+    wavelength: np.ndarray = np.array([0.9, 1.6])
+    efficiency: np.ndarray = np.array([0.85, 0.85])
     title:      str = 'Default'
     comment:    str = 'default value'
 
     @classmethod
     def from_json(cls, filename):
-        """
-        This method creates efficiency array data depend on wavelength
+        """This method creates efficiency array data depend on wavelength.
 
         Args:
             filename: Input json filename.
 
         Returns:
-            WlEfic: Created efficiency object.
+            Efficiency: Created efficiency object.
         """
-        with open(filename, "r") as fp:
+        with open(filename, 'r') as fp:
             js = json.load(fp)
             wavelength = np.array(js['wavelength'])
             efficiency = np.array(js['efficiency'])
@@ -38,10 +38,10 @@ class WlEfic:
             if 'comment' in js:
                 comment = js['comment']
 
-        wlefic = WlEfic(
-            wavelength = wavelength,
-            efficiency = efficiency,
-            title      = title,
-            comment    = comment)
+        wlefic = Efficiency(
+            wavelength=wavelength,
+            efficiency=efficiency,
+            title=title,
+            comment=comment)
 
         return wlefic
--- src/telescope_baseline/utils/plot_eff.py
+++ src/telescope_baseline/utils/plot_eff.py
@@ -2,7 +2,7 @@
 """Plot efficinecy
 
     usage:
-        plot_eff.py [-h|--help] -i eff.json -p plotfile.png
+        plot_eff.py [-h|--help] -i eff.json [-p plotfile.png]
 
     options:
         --help            show this help message and exit
@@ -10,26 +10,30 @@
         -p plotfile.png   Output file
 """
 from docopt import docopt                         # command line interface
-import numpy as np
 import matplotlib.pyplot as plt
-from telescope_baseline.dataclass.wl_eff import WlEfic
+from telescope_baseline.dataclass.efficiency import Efficiency
 
 if __name__ == '__main__':
     args = docopt(__doc__)
+    input_file = args['-i']
+    if(args['-p']):
+        plotfile = args['-p']
+    else:
+        plotfile = False
 
-input_file = args['-i']
+    efficiency = Efficiency.from_json(input_file)
 
-wl_eff = WlEfic.from_json(input_file)
-
-fig = plt.figure()
-ax = fig.add_subplot(1,1,1)
-ax.set_xlim(0.8,1.7)
-ax.set_ylim(0.0,1)
-ax.set_xlabel('Wavelength ($\mu$m)')
-ax.set_ylabel('Efficiency')
-ax.set_title(wl_eff.title)
-ax.text(0.85,0.50,wl_eff.comment,size=7)
-ax.plot(wl_eff.wavelength,wl_eff.efficiency,label=wl_eff.title) 
-ax.legend()
-plt.show()
-fig.savefig(args['-p'],transparent=True)
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_xlim(0.8, 1.7)
+    ax.set_ylim(0.0, 1)
+    ax.set_xlabel('Wavelength ($\\mu$m)')
+    ax.set_ylabel('Efficiency')
+    ax.set_title(efficiency.title)
+    ax.text(0.85, 0.50, efficiency.comment, size=7)
+    ax.plot(efficiency.wavelength, efficiency.efficiency,
+            label=efficiency.title)
+    ax.legend()
+    plt.show()
+    if(plotfile):
+        fig.savefig(plotfile, transparent=True)
