2f4bba018d615e1580410dba3472596244043bfd
tkamizuka <kamizuka@ioa.s.u-tokyo.ac.jp>
2022-05-22 21:38:54 +0900

    filters moved from data to tools (TK).

--- src/telescope_baseline/tools/efficiency/filters.py
+++ src/telescope_baseline/tools/efficiency/filters.py
@@ -1,12 +1,15 @@
-class __Filters:
+import os
+import pathlib
+import pkg_resources
+from telescope_baseline.dataclass.efficiency import Efficiency
+
+class Filters:
     """This class handles the filter data stored in the data directory.
 
     Attributes:
         json_list (list): List of the json files
     """
     def __init__(self):
-        import pkg_resources
-        import pathlib
         self.__data_path = pkg_resources.resource_filename('telescope_baseline', 'data/filter')
         self.json_list = []
         for p in pathlib.Path(self.__data_path).glob('*.json'):
@@ -22,9 +25,4 @@ class __Filters:
         Returns:
             efficiency: Efficiency instance created from the input json file.
         """
-        import os
-        from telescope_baseline.dataclass.efficiency import Efficiency
         return Efficiency.from_json(os.path.join(self.__data_path, json_name))
-
-
-filters = __Filters()
